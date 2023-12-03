544bf7b6ceb9709f00c55e908fe1b30f070fac14
khawlinkoon <64354227+khawlinkoon@users.noreply.github.com>
2023-12-03 18:05:22 +0800

    Added various clustering algorithms
    - Change json to yaml
    - Added following algorithms:
    1) MiniKmeans
    2) Affinity Propagation
    3) Birch
    4) Spectral Clustering
    5) Gaussian Mixture
    6) Latent Dirichlet Allocation
    7) Hidden Markov

--- src/ClusterAlgo.py
+++ src/ClusterAlgo.py
@@ -1,5 +1,9 @@
 import numpy as np
-from sklearn.cluster import KMeans, DBSCAN
+from sklearn.cluster import KMeans, DBSCAN, MiniBatchKMeans, AffinityPropagation, Birch, SpectralClustering
+from sklearn.mixture import GaussianMixture
+from sklearn.decomposition import LatentDirichletAllocation
+from hmmlearn.hmm import GaussianHMM
+
 from src.ClusterMember import ClusterMember, ClusterMemberStats
 
 class Kmeans:
@@ -27,13 +31,18 @@ class Kmeans:
         self.stats.setBaseStation(self.model.labels_)
         return self.model
     
+    def getLabels(
+        self,
+    ) -> list:
+        return self.model.labels_
+    
     # TODO implement
     def getOptimalCluster(
         self,
     ) -> int:
         return 5
 
-
+# TODO: Test different parameters
 class DensityBased:
     def setData(
         self,
@@ -52,3 +61,187 @@ class DensityBased:
         self.model.fit(self.data)
         self.stats.setBaseStation(self.model.labels_)
         return self.model
+    
+    def getLabels(
+        self,
+    ) -> list:
+        return self.model.labels_
+
+class MiniKmeans:
+    def setData(
+        self,
+        stats: ClusterMemberStats,
+    ) -> None:
+        self.stats = stats
+
+        self.data = self.stats.getPosition()
+
+    def generateModel(
+        self,
+        optimal : bool = False,
+        n_clusters : int = 5,
+        n_init : int = 10,
+    ) -> MiniBatchKMeans:
+        if n_clusters != 5:
+            self.model = MiniBatchKMeans(n_clusters=n_clusters, n_init=n_init)
+        elif optimal:
+            self.model = MiniBatchKMeans(n_clusters=self.getOptimalCluster(), n_init=n_init)
+        else:
+            self.model = MiniBatchKMeans(n_clusters=n_clusters, n_init=n_init)
+        self.model.fit(self.data)
+        self.stats.setBaseStation(self.model.labels_)
+        return self.model
+    
+    # TODO implement
+    def getOptimalCluster(
+        self,
+    ) -> int:
+        return 5
+    
+    def getLabels(
+        self,
+    ) -> list:
+        return self.model.labels_
+
+class AffinityProp:
+    def setData(
+        self,
+        stats: ClusterMemberStats,
+    ) -> None:
+        self.stats = stats
+
+        self.data = self.stats.getPosition()
+
+    def generateModel(
+        self,
+        damping : float = 0.9,
+    ) -> AffinityPropagation:
+        self.model = AffinityPropagation(damping=damping)
+        self.model.fit(self.data)
+        self.stats.setBaseStation(self.model.labels_)
+        return self.model
+    
+    def getLabels(
+        self,
+    ) -> list:
+        return self.model.labels_
+
+class Balanced:
+    def setData(
+        self,
+        stats: ClusterMemberStats,
+    ) -> None:
+        self.stats = stats
+
+        self.data = self.stats.getPosition()
+
+    def generateModel(
+        self,
+        threshold: float = 0.01,
+        n_clusters : int = 5,
+    ) -> Birch:
+        self.model = Birch(threshold=threshold, n_clusters=n_clusters)
+        self.model.fit(self.data)
+        self.stats.setBaseStation(self.model.labels_)
+        return self.model
+    
+    def getLabels(
+        self,
+    ) -> list:
+        return self.model.labels_
+
+# TODO: check why spectral why cant work with random data?
+class Spectral:
+    def setData(
+        self,
+        stats: ClusterMemberStats,
+    ) -> None:
+        self.stats = stats
+
+        self.data = self.stats.getPosition()
+
+    def generateModel(
+        self,
+        n_clusters : int = 5,
+    ) -> SpectralClustering:
+        self.model = SpectralClustering(n_clusters=n_clusters)
+        self.model.fit(self.data)
+        self.stats.setBaseStation(self.model.labels_)
+        return self.model
+    
+    def getLabels(
+        self,
+    ) -> list:
+        return self.model.labels_
+
+class Gaussian:
+    def setData(
+        self,
+        stats: ClusterMemberStats,
+    ) -> None:
+        self.stats = stats
+
+        self.data = self.stats.getPosition()
+
+    def generateModel(
+        self,
+        n_components : int = 5,
+    ) -> GaussianMixture:
+        self.model = GaussianMixture(n_components=n_components)
+        self.model.fit(self.data)
+        self.stats.setBaseStation(self.model.predict(self.data))
+        return self.model
+
+    def getLabels(
+        self,
+    ) -> list:
+        return self.model.predict(self.data)
+
+# TODO: How to make clsuter an interger not float
+class LDA:
+    def setData(
+        self,
+        stats: ClusterMemberStats,
+    ) -> None:
+        self.stats = stats
+
+        self.data = self.stats.getPosition()
+
+    def generateModel(
+        self,
+        n_components : int = 5,
+        random_state: int = 0,
+    ) -> LatentDirichletAllocation:
+        self.model = LatentDirichletAllocation(n_components=n_components, random_state=random_state)
+        self.model.fit(self.data)
+        self.stats.setBaseStation(self.model.transform(self.data))
+        return self.model
+
+    def getLabels(
+        self,
+    ) -> list:
+        return self.model.transform(self.data)
+
+class Markov:
+    def setData(
+        self,
+        stats: ClusterMemberStats,
+    ) -> None:
+        self.stats = stats
+
+        self.data = self.stats.getPosition()
+
+    def generateModel(
+        self,
+        n_components : int = 5,
+    ) -> GaussianHMM:
+        self.model = GaussianHMM(n_components=n_components)
+        self.model.fit(self.data)
+        self.stats.setBaseStation(self.model.predict(self.data))
+        return self.model
+
+    def getLabels(
+        self,
+    ) -> list:
+        return self.model.predict(self.data)
+
--- src/Simulation.py
+++ src/Simulation.py
@@ -1,12 +1,14 @@
-import json
+import yaml
 import numpy as np
 import logging
+import time
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
 from matplotlib.animation import FuncAnimation
+import matplotlib.patches as mpatches
 
 from src.utils import Mobility, Distribution
-from src.ClusterAlgo import Kmeans, DensityBased
+from src.ClusterAlgo import Kmeans, DensityBased, MiniKmeans, AffinityProp, Balanced, Spectral, Gaussian, LDA, Markov
 from src.ClusterMember import ClusterMember, ClusterMemberStats
 from src.ClusterHead import ClusterHead
 
@@ -18,8 +20,8 @@ class Simulation:
         self.initialize()
     
     def getConfig(self) -> None:
-        with open("config.json") as f:
-            self.config = json.load(f)
+        with open("config.yml") as f:
+            self.config = yaml.safe_load(f)
 
     def initialize(self) -> None:
         self.getConfig()
@@ -34,20 +36,15 @@ class Simulation:
         )
         self.cluster_head = []
         self.cluster_member_stats = ClusterMemberStats(self.cluster_member)
+        self.total_runtime = "0 ms"
+        self.initial_runtime = ""
     
     def getClusteringAlgo(self):
-        currentAlgorithm = self.config["algorithm"].lower()
-
-        if currentAlgorithm == "kmeans":
-            algo = Kmeans()
-            algo.setData(self.cluster_member_stats)
-            number_of_cluster = 5
-            self.clustering = algo.generateModel(n_clusters = number_of_cluster)
-            
-            self.center_x, self.center_y, self.center_z = self.clustering.cluster_centers_.T
-            max_distance = self.cluster_member_stats.maxDistance(self.clustering.cluster_centers_)
+        def setClusterHeadWithCenters(cluster_center: list) -> None:
+            self.center_x, self.center_y, self.center_z = cluster_center.T
+            max_distance = self.cluster_member_stats.maxDistance(cluster_center)
             self.cluster_head = []
-            for ind in range(number_of_cluster):
+            for ind in range(len(np.unique(self.labels))):
                 head = ClusterHead(
                     id = ind,
                     position = [self.center_x[ind],self.center_y[ind],100],
@@ -57,32 +54,28 @@ class Simulation:
                     energy = self.config["uav_range"]
                 )
                 self.cluster_head.append(head)
-        elif currentAlgorithm == "density-based":
-            algo = DensityBased()
-            algo.setData(self.cluster_member_stats)
-            # self.clustering = algo.generateModel(eps = self.config['uav_range'])
-            self.clustering = algo.generateModel(eps = 100)
 
-            temp_center_x = [[] for ind in range(len(np.unique(self.clustering.labels_)))]
-            temp_center_y = [[] for ind in range(len(np.unique(self.clustering.labels_)))]
-            temp_center_z = [[] for ind in range(len(np.unique(self.clustering.labels_)))]
+        def setClusterHeadWithoutCenters() -> None:
+            temp_center_x = [[] for ind in range(len(np.unique(self.labels)))]
+            temp_center_y = [[] for ind in range(len(np.unique(self.labels)))]
+            temp_center_z = [[] for ind in range(len(np.unique(self.labels)))]
             
             for cluster in self.cluster_member:
                 temp_center_x[cluster.base_station].append(cluster.position[0])
                 temp_center_y[cluster.base_station].append(cluster.position[1])
                 temp_center_z[cluster.base_station].append(cluster.position[2])
             
-            self.center_x = [0 for ind in range(len(np.unique(self.clustering.labels_)))]
-            self.center_y = [0 for ind in range(len(np.unique(self.clustering.labels_)))]
-            self.center_z = [0 for ind in range(len(np.unique(self.clustering.labels_)))]
-            for ind in range(len(np.unique(self.clustering.labels_))):
+            self.center_x = [0 for ind in range(len(np.unique(self.labels)))]
+            self.center_y = [0 for ind in range(len(np.unique(self.labels)))]
+            self.center_z = [0 for ind in range(len(np.unique(self.labels)))]
+            for ind in range(len(np.unique(self.labels))):
                 self.center_x[ind] = sum(temp_center_x[ind])/len(temp_center_x[ind]) if len(temp_center_x[ind]) != 0 else 0
                 self.center_y[ind] = sum(temp_center_y[ind])/len(temp_center_y[ind]) if len(temp_center_y[ind]) != 0 else 0
                 self.center_z[ind] = sum(temp_center_z[ind])/len(temp_center_z[ind]) if len(temp_center_z[ind]) != 0 else 0
             
             max_distance = self.cluster_member_stats.maxDistance(np.array([self.center_x,self.center_y,self.center_z]).T)
             self.cluster_head = []
-            for ind in range(len(np.unique(self.clustering.labels_))):
+            for ind in range(len(np.unique(self.labels))):
                 head = ClusterHead(
                     id = ind,
                     position = [self.center_x[ind],self.center_y[ind],100],
@@ -93,8 +86,69 @@ class Simulation:
                 )
                 self.cluster_head.append(head)
 
+        current_algorithm = self.config["algorithm"].lower()
+        start_time = time.time()
+        if current_algorithm == "kmeans":
+            algo = Kmeans()
+            algo.setData(self.cluster_member_stats)
+            self.clustering = algo.generateModel(n_clusters = 5)
+            self.labels = algo.getLabels()
+            setClusterHeadWithCenters(cluster_center=self.clustering.cluster_centers_)
+        elif current_algorithm == "mini_kmeans":
+            algo = MiniKmeans()
+            algo.setData(self.cluster_member_stats)
+            self.clustering = algo.generateModel(n_clusters = 5)
+            self.labels = algo.getLabels()
+            setClusterHeadWithCenters(cluster_center=self.clustering.cluster_centers_)
+        elif current_algorithm == "density_based":
+            algo = DensityBased()
+            algo.setData(self.cluster_member_stats)
+            self.clustering = algo.generateModel(eps = 20)
+            self.labels = algo.getLabels()
+            setClusterHeadWithoutCenters()
+        elif current_algorithm == "affinity_propagation":
+            algo = AffinityProp()
+            algo.setData(self.cluster_member_stats)
+            self.clustering = algo.generateModel(damping=0.9)
+            self.labels = algo.getLabels()
+            setClusterHeadWithoutCenters()
+        elif current_algorithm == "birch":
+            algo = Balanced()
+            algo.setData(self.cluster_member_stats)
+            self.clustering = algo.generateModel(threshold=0.01, n_clusters=5)
+            self.labels = algo.getLabels()
+            setClusterHeadWithoutCenters()
+        elif current_algorithm == "spectral":
+            algo = Spectral()
+            algo.setData(self.cluster_member_stats)
+            self.clustering = algo.generateModel(n_clusters=5)
+            self.labels = algo.getLabels()
+            setClusterHeadWithoutCenters()
+        elif current_algorithm == "gaussian":
+            algo = Gaussian()
+            algo.setData(self.cluster_member_stats)
+            self.clustering = algo.generateModel(n_components=5)
+            self.labels = algo.getLabels()
+            setClusterHeadWithoutCenters()
+        elif current_algorithm == "lda":
+            return NotImplementedError
+            algo = LDA()
+            algo.setData(self.cluster_member_stats)
+            self.clustering = algo.generateModel(n_components=5, random_state=0)
+            self.labels = algo.getLabels()
+            setClusterHeadWithoutCenters()
+        elif current_algorithm == "markov":
+            algo = Markov()
+            algo.setData(self.cluster_member_stats)
+            self.clustering = algo.generateModel(n_components=5)
+            self.labels = algo.getLabels()
+            setClusterHeadWithoutCenters()
         else:
             return NotImplementedError
+        
+        self.total_runtime = f"{(time.time()-start_time)*1000:.2f} ms"
+        if self.initial_runtime == "":
+            self.initial_runtime = self.total_runtime
     
     def run(self) -> None:
         self.setFigure()
@@ -104,9 +158,9 @@ class Simulation:
         self.ani = FuncAnimation(
             self.fig, 
             self.updateGraph, 
-            frames=21, 
+            frames=self.config["cycle_frames"] + 1, 
             init_func=self.draw, 
-            interval=500
+            interval=self.config['time_per_frame']
         )
         plt.show()
 
@@ -114,6 +168,7 @@ class Simulation:
         self.fig = plt.figure(figsize=(14,7))
         self.fig.suptitle("Wireless Communication in Disaster Region Simulation", size=16)
         self.fig.set(tight_layout=True)
+
         self.gs = gridspec.GridSpec(3, 2, width_ratios=[7,3])
         self.ax = [
             plt.subplot(self.gs[:,0]),
@@ -142,7 +197,14 @@ class Simulation:
         pos = np.array(self.cluster_member_stats.getPosition())
         x,y,z = pos.T
         if update:
-            self.ax[0].scatter(x,y,c=self.clustering.labels_)
+            colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
+            color_label = [colors[label%10] for label in self.labels]
+            self.ax[0].scatter(x, y, c= color_label)
+            
+            patch = []
+            for ind, group in enumerate(np.unique([f"Cluster {label+1}" for label in self.labels])):
+                patch.append(mpatches.Patch(color=colors[ind%10], label=group))
+            self.ax[0].legend(handles=patch, loc="lower right")
 
             self.ax[0].scatter(
                 self.center_x, 
@@ -180,10 +242,10 @@ class Simulation:
         self.graph1_x = [ind for ind in range(len(self.graph1_y))]
 
         self.ax[1].plot(self.graph1_x,self.graph1_y)
-        self.ax[1].set_xlim([0,20])
+        self.ax[1].set_xlim([0,self.config["cycle_frames"]])
         self.ax[1].set_ylim([0,100])
         self.ax[1].set_title("Connectivity Ratio")
-        self.ax[1].legend([f"UAV {i+1}" for i in range(len(self.cluster_head))], loc="lower right")
+        # self.ax[1].legend([f"UAV {i+1}" for i in range(len(self.cluster_head))], loc="lower right")
 
     def drawGraph2(self) -> None:
         self.ax[2].cla()
@@ -196,31 +258,39 @@ class Simulation:
         self.graph2_x = [ind for ind in range(len(self.graph2_y))]
         
         self.ax[2].plot(self.graph2_x,self.graph2_y)
-        self.ax[2].set_xlim([0,20])
+        self.ax[2].set_xlim([0,self.config["cycle_frames"]])
         self.ax[2].set_ylim([80,100])
         self.ax[2].set_title("Uplink Probability")
-        self.ax[2].legend([f"UAV {i+1}" for i in range(len(self.cluster_head))], loc="lower right")
+        # self.ax[2].legend([f"UAV {i+1}" for i in range(len(self.cluster_head))], loc="lower right")
     
     def drawGraph3(self) -> None:
         self.ax[3].cla()
 
         read_input = [
-            "10"
+            self.config["algorithm"].replace("_"," ").title(),
+            self.initial_runtime.format(len(self.config["algorithm"])),
+            self.total_runtime.format(len(self.config["algorithm"])),
         ]
 
         row_labels = [
-            "Algorithm runtime", 
+            "Algorithm                ",
+            "Algorithm initial runtime", 
+            "Algorithm runtime        ", 
         ]
 
-        cell_text = [
-            [f"{read_input[0]} s"],
-        ]
+        cell_text = [[text] for text in read_input]
 
         self.ax[3].axis("off")
         self.ax[3].axis("tight")
         self.ax[3].tick_params(axis='x', which='major', pad=15)
-        # self.ax[3].set_title("Data")
-        self.ax[3].table(cellText=cell_text, rowLabels=row_labels, colWidths=[0.35, 0.2], loc='center right')
+        self.ax[3].set_title("Data")
+        table = self.ax[3].table(
+            cellText = cell_text, 
+            rowLabels = row_labels, 
+            colWidths = [0.5, 0.4], 
+            loc = 'best')
+        table.auto_set_font_size(False)
+        table.set_fontsize(11)
     
 def initializeClusterMembers(
     length: float,
@@ -256,3 +326,4 @@ def initializeClusterMembers(
         all_cluster_members.append(cluster_member)
 
     return all_cluster_members
+
